717ab4931cb2f4a5b1d4539502069fa6fb90a138
Steven DOng <steven.dong@ibm.com>
2021-09-15 18:17:30 -0400

    clean up code

--- evendisplay.go
+++ evendisplay.go
@@ -15,16 +15,11 @@ import (
 	"sync/atomic"
 	"time"
 
+	"go.uber.org/zap"
+	"knative.dev/pkg/logging"
+
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
-
-var msg struct {
-	data          map[string](map[int](map[string]time.Time))
-	Mu            sync.Mutex
-	totalMessages int
-	dups          int
-	timestamp     time.Time
-}
 
 var msg1 struct {
 	data      []string
@@ -36,6 +31,10 @@ var httpClient http.Client
 var delay = getenv("delay", "3")
 var scraperAdd = getenv("scraper", "")
 var connCt uint64
+var logger *zap.SugaredLogger
+var forward = os.Getenv("forward") == "true"
+var displayConcurrentConnections = os.Getenv("displayConcurrentConnections") == "true"
+var cloudevent = os.Getenv("cloudevent") == "true"
 
 func getenv(key, fallback string) string {
 	value := os.Getenv(key)
@@ -47,17 +46,17 @@ func getenv(key, fallback string) string {
 
 func processCloudEvents(event cloudevents.Event) {
 	atomic.AddUint64(&connCt, 1)
-	l := time.Now()
 	d, _ := strconv.Atoi(delay)
 	time.Sleep(time.Second * time.Duration(d))
 	eventInfoArray := strings.Split(event.Context.GetID(), "/")
-	topic := strings.Split(event.Context.GetSource(), "#")[1]
+	cesource := strings.Split(event.Context.GetSource(), "#")
+	topic := cesource[1]
+	kafkasourcearray := strings.Split(cesource[0], "/")
+	source := kafkasourcearray[len(kafkasourcearray)-1]
 	partition, offset := strings.Split(eventInfoArray[0], ":")[1], strings.Split(eventInfoArray[1], ":")[1]
-	p, err := strconv.Atoi(partition)
-	if err != nil {
-		fmt.Println("An error happened when converting partition to integer")
+	if forward {
+		appendToData2(source, topic, partition, offset)
 	}
-	appendToData(topic, p, offset, l)
 	atomic.AddUint64(&connCt, ^uint64(1-1))
 }
 
@@ -69,69 +68,58 @@ func processEvents(w http.ResponseWriter, r *http.Request) {
 	}
 	partitionAndOffset := r.Header.Get("Ce-Id")
 	eventInfoArray := strings.Split(partitionAndOffset, "/")
-	topic := strings.Split(r.Header.Get("Ce-Source"), "#")[1]
+	cesource := strings.Split(r.Header.Get("Ce-Source"), "#")
+	topic := cesource[1]
+	kafkasourcearray := strings.Split(cesource[0], "/")
+	source := kafkasourcearray[len(kafkasourcearray)-1]
 	partition, offset := strings.Split(eventInfoArray[0], ":")[1], strings.Split(eventInfoArray[1], ":")[1]
-	appendToData2(topic, partition, offset)
+	if forward {
+		appendToData2(source, topic, partition, offset)
+	}
+	printout := fmt.Sprintf("%s/%s/%s/%s", source, topic, partition, offset)
+	w.Write([]byte(printout))
 	atomic.AddUint64(&connCt, ^uint64(1-1))
 }
 
-func appendToData2(t string, p string, o string) {
+func appendToData2(source string, t string, p string, o string) {
 	msg1.Mu.Lock()
-	s := t + "/" + p + "/" + o
+	s := source + "/" + t + "/" + p + "/" + o + "/" + "200"
 	msg1.data = append(msg1.data, s)
+	msg1.timestamp = time.Now()
 	msg1.Mu.Unlock()
 }
 
-func appendToData(topic string, partition int, offset string, t time.Time) {
-	msg.Mu.Lock()
-	if partitions, found := msg.data[topic]; found {
-		if offsets, ok := partitions[partition]; ok {
-			if _, ok := offsets[offset]; ok {
-				msg.dups += 1
-			} else {
-				offsets[offset] = t
-				msg.totalMessages += 1
-			}
-		} else {
-			newSetOfOffsets := make(map[string]time.Time)
-			newSetOfOffsets[offset] = t
-			partitions[partition] = newSetOfOffsets
-			msg.totalMessages += 1
-		}
-	} else {
-		newSetOfOffsets := make(map[string]time.Time)
-		newSetOfOffsets[offset] = t
-		newPartitions := make(map[int](map[string]time.Time))
-		newPartitions[partition] = newSetOfOffsets
-		msg.data[topic] = newPartitions
-		msg.totalMessages += 1
-	}
-	msg.Mu.Unlock()
-}
-
 func main() {
-	if msg.data == nil {
-		msg.data = make(map[string](map[int](map[string]time.Time)))
+	ctx, _ := context.WithCancel(context.Background())
+	logger = logging.FromContext(ctx)
+	logger.Infof("Env vars: displayConcurrentConnection: %t delay: %s cloudevent: %t forward: %t delay: %s scraper: %s \n", displayConcurrentConnections, delay, cloudevent, forward, scraperAdd)
+
+	if displayConcurrentConnections {
+		go displayCon()
 	}
-	fmt.Println("Starting receiver...")
-	go displayCon()
-	go sendResult2()
-	if os.Getenv("cloudevent") == "true" {
+
+	if forward {
+		go sendResult2()
+	}
+
+	if cloudevent {
+		logger.Info("Receiving events as cloudevents")
 		c, err := cloudevents.NewDefaultClient()
 		if err != nil {
 			log.Fatal("Failed to create client, ", err)
 		}
 		log.Fatal(c.StartReceiver(context.Background(), processCloudEvents))
 	} else {
+		logger.Info("Receiving events as regular http requests")
 		http.HandleFunc("/", processEvents)
 		port := os.Getenv("PORT")
 		if port == "" {
 			port = "8080"
 		}
-		log.Printf("listening on port %s", port)
 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 	}
 }
+
 func sendResult2() {
 	for {
 		time.Sleep(time.Second * 5)
@@ -140,62 +128,73 @@ func sendResult2() {
 		msg1.data = []string{}
 		msg1.Mu.Unlock()
 		if err != nil {
-			fmt.Println("error streaming data", err.Error())
+			logger.Infof("Error streaming data: %s", err.Error())
 			return
 		}
 		if scraperAdd == "" {
-			fmt.Println("scraper address was not correct configured, check env setting for scraper")
+			logger.Info("scraper address was not correct configured, check env setting for scraper")
 			return
 		}
-		resp, err := http.Post(scraperAdd, "application/json", bytes.NewBuffer(data))
-		if err != nil {
-			fmt.Println("error sending data", err.Error())
-			return
-		}
-		responseString, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("err reading response body", err.Error())
-			return
-		}
-		fmt.Println("Rcv'd response from scraper: \n", string(responseString))
-	}
-}
 
-func sendResult() {
-	for {
-		time.Sleep(time.Second * 5)
-		msg.Mu.Lock()
-		i := time.Now()
-		data, err := json.Marshal(msg.data)
-		msg.data = make(map[string]map[int]map[string]time.Time)
-		fmt.Println("marshal:", int(time.Since(i).Seconds()))
-		msg.Mu.Unlock()
-		if err != nil {
-			fmt.Println("error streaming data", err.Error())
-			return
+		retry := 0
+		for retry < 3 {
+			resp, err := http.Post(scraperAdd, "application/json", bytes.NewBuffer(data))
+			if err != nil {
+				logger.Infof("error sending data: %s\n", err.Error())
+				retry++
+				time.Sleep(time.Second * 2)
+			} else {
+				retry = 3
+				responseString, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					logger.Infof("Err reading response body: %s", err.Error())
+				}
+				logger.Infof("%s rcv'd response from scraper status code: %d content: %s\n", os.Getenv("HOSTNAME"), resp.StatusCode, responseString)
+				resp.Body.Close()
+			}
 		}
-		if scraperAdd == "" {
-			fmt.Println("scraper address was not correct configured, check env setting for scraper")
-			return
-		}
-		resp, err := http.Post(scraperAdd, "application/json", bytes.NewBuffer(data))
-		if err != nil {
-			fmt.Println("error sending data", err.Error())
-			return
-		}
-		responseString, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("err reading response body", err.Error())
-			return
-		}
-		fmt.Println("Rcv'd response from scraper: \n", string(responseString))
 	}
 }
 
 func displayCon() {
 	for {
 		time.Sleep(1 * time.Second)
-		fmt.Printf("Host %s, Currently concurrent connection number %d connCt\n", os.Getenv("HOSTNAME"), connCt)
-		fmt.Print("------------------------------------------------------------\n")
+		logger.Infof("Host: %s: we have %d concurrent connections\n", os.Getenv("HOSTNAME"), connCt)
 	}
 }
+
+// var msg struct {
+// 	data          map[string](map[int](map[string]time.Time))
+// 	Mu            sync.Mutex
+// 	totalMessages int
+// 	dups          int
+// 	timestamp     time.Time
+// }
+
+// func appendToData(topic string, partition int, offset string, t time.Time) {
+// 	msg.Mu.Lock()
+// 	if partitions, found := msg.data[topic]; found {
+// 		if offsets, ok := partitions[partition]; ok {
+// 			if _, ok := offsets[offset]; ok {
+// 				msg.dups += 1
+// 			} else {
+// 				offsets[offset] = t
+// 				msg.totalMessages += 1
+// 			}
+// 		} else {
+// 			newSetOfOffsets := make(map[string]time.Time)
+// 			newSetOfOffsets[offset] = t
+// 			partitions[partition] = newSetOfOffsets
+// 			msg.totalMessages += 1
+// 		}
+// 	} else {
+// 		newSetOfOffsets := make(map[string]time.Time)
+// 		newSetOfOffsets[offset] = t
+// 		newPartitions := make(map[int](map[string]time.Time))
+// 		newPartitions[partition] = newSetOfOffsets
+// 		msg.data[topic] = newPartitions
+// 		msg.totalMessages += 1
+// 	}
+// 	msg.timestamp = time.Now()
+// 	msg.Mu.Unlock()
+// }
--- go.mod
+++ go.mod
@@ -3,5 +3,10 @@ module github.com/steven0711dong/customizeEventDisplay
 go 1.14
 
 require (
-	github.com/cloudevents/sdk-go/v2 v2.2.0
+	github.com/cloudevents/sdk-go/v2 v2.5.0
+	go.uber.org/zap v1.17.0
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	knative.dev/pkg v0.0.0-20210624121345-a1bc850346b8
 )
